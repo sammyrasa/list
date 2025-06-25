3f67a419403040a87bfb2c2b967eeb3669f160db
caetanaldo <gustavocaetanodalponte@gmail.com>
2025-06-25 09:51:29 -0300

    Update App.js

---
+++ lista/App.js
@@ -0,0 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
